3780d8882dfd37d64906e33b8a75142413ec2e36
Jimmy Rigby <jrigby@pros.com>
2019-09-20 09:37:57 -0500

    Updated and cleaned

--- Gradient Decent/Logistic Regression Gradient Decent.R
+++ Gradient Decent/Logistic Regression Gradient Decent.R
@@ -1,47 +1,80 @@
+
+# Loading dependencies and data -------------------------------------------
 library(ISLR)
+
 test<-Smarket
 
+# Coding outcome as 1
 test<-test%>%
   mutate(Up = if_else(Direction=="Up", 1, 0 ))
 
+# Writing GLM function
 my_glm<-function(formula=NULL, y=NULL, x=NULL, data =NULL){
-  # Designing Model Matricies
+  # Designing Model Matricies x and y using x and y arguments
   if(!is.null(y) & !is.null(x)){
     x<-as.matrix(cbind(1, x))
     colnames(x)[1]<-"(Intercept)"
     y<-as.matrix(y)
+    
+  #Designing model Matrices using formula
   } else if(!is.null(formula) & !is.null(data)){
     x<-model.matrix(formula, data)
-    y<-as.matrix(model.frame(formula, data)[,!all.vars(formula)   %in% all.vars(formula[[3]])])
+    y<-as.matrix(model.frame(formula, data)[,!all.vars(formula) %in% all.vars(formula[[3]])])
+    
+  # Throws an error if neither x and y are provided nor formula and data
   }else{
     stop("Please provide either a formula and data or x and y.")
   }
   
+  # Initialized theta
   theta<-rep(0, ncol(x))
   
+  # Defines model
   model<- function(theta) 1/(1+exp(-(x%*%theta)))
+  
+  # Defines loss function
   cost<- function(theta){
     g<-model(theta)
-    -(1/nrow(x))* sum((y*log(g))+((1-y)*log(1-g)))
+    -(1/nrow(x)) * sum((y*log(g))+((1-y)*log(1-g)))
   }
 
- # optim(par = theta, fn = cost)
-  
-optim(theta, cost)
+# Optimizes theta
+optim_theta<-optim(theta, cost, method = "BFGS", hessian = TRUE)
+
+par <- optim_theta$par
+vcov <- optim_theta$hessian
+se <- sqrt(diag(vcov))
+
+
+names(par)<-colnames(x)
+
+return(list(parameters = par,
+            se = se,
+            z = par/se,
+            hess = vcov))
 }
 
 
 # R bloggers implementation -----------------------------------------------
 
 
-X<-as.matrix(test%>%mutate(one = 1)%>%select(one, Lag1, Lag2, Lag3))
-Y<-as.matrix(test%>%select(Up))
+X<-test%>%
+  mutate(one = 1)%>%
+  select(one, Lag1, Lag2, Lag3)%>%
+  as.matrix(.)
+
+Y<-test%>%
+  select(Up)%>%
+  as.matrix(.)
+
+# Defining model
 sigmoid <- function(z)
 {
   g <- 1/(1+exp(-z))
   return(g)
 }
 
+# Defining loss function
 cost <- function(theta)
 {
   m <- nrow(X)
@@ -50,15 +83,13 @@ cost <- function(theta)
   return(J)
 }
 
-
-
+# initializing theta
 initial_theta <- rep(0,ncol(X))
 
-cost(initial_theta)
-#
-optim(initial_theta, fn = cost)
+# Optimizing theta
+optim(initial_theta, fn = cost, method = "BFGS", hessian = TRUE)
 
 # Comprison of Approaches -------------------------------------------------
 my_glm(Up ~ Lag1+Lag2+Lag3, data = test)
 
-glm(Up ~ Lag1+Lag2+Lag3 ,data = test, family = "binomial")
+summary(glm(Up ~ Lag1+Lag2+Lag3 ,data = test, family = "binomial"))
