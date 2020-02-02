0337a6f28ad4b88c1fbc1457c97338bff955c7b9
Jimmy Rigby <jrigby@pros.com>
2020-02-02 11:11:11 -0600

    started GP application predicting  seasonal flows

--- Gaussian Proccesses/Gaussian Process Application.R
+++ Gaussian Proccesses/Gaussian Process Application.R
@@ -1,10 +1,11 @@
+library(tidyverse)
 sq_exp_kernel<-function(x1, x2, l = 1){
   Sigma<-matrix(rep(0, length(x1)*length(x2)), nrow = length(x1))
   
   for (i in seq_along(x1)){
     for (j in seq_along(x2)){
       
-      Sigma[i, j]<-exp(-0.5*(abs(x2[j]-x1[i])/l)^2)
+      Sigma[i, j]<-exp(-0.5*(abs(x2[i]-x1[j])/l)^2)
       
     }
   }
@@ -13,19 +14,7 @@ sq_exp_kernel<-function(x1, x2, l = 1){
 }
 
 
-seasonality_kernel<-function(x1, x2, a=1, b=12, l = 1){
-  Sigma<-matrix(rep(0, length(x1)*length(x2)), nrow = length(x1))
-  
-  for (i in seq_along(x1)){
-    for (j in seq_along(x2)){
-      
-      Sigma[i, j]<-a^2*exp(-0.5*(sin(b*(x2[j]-x1[i]))/l)^2)
-      
-    }
-  }
-  
-  Sigma
-}
+
 
 draw<-function(n_draws, x, mu, cov){
   values<-map_dfr(1:n_draws, function(sample){
@@ -64,25 +53,42 @@ post<-function(x, x.star, y, sigma.n=0, kernel = sq_exp_kernel){
        post_var = f.star.cov)
 }
 
-cisco_cfs <- read_delim("C:/Users/jimmy/Downloads/Cisco_cfs.tab",
+cisco_cfs <- read_delim("Gaussian Proccesses/Cisco_cfs.tab",
                         "\t", 
                         escape_double = FALSE, 
                         trim_ws = TRUE)
 
 cisco_cfs<-cisco_cfs%>%
-  mutate(day = 1:n(), 
-         year = lubridate::year(date))%>%
-  dplyr::select(date, year, day, cfs)
+  mutate(year = lubridate::year(date))%>%
+  dplyr::select(date, year, cfs)%>%
+  arrange(date)%>%
+  group_by(year)%>%
+  mutate(day = 1:n())%>%
+  ungroup()
 
 missing<-(1:nrow(cisco_cfs))[is.na(cisco_cfs$cfs)]
 
 cisco_cfs$cfs[missing]<-cisco_cfs$cfs[missing-1]
 
+cisco_cfs<-split(cisco_cfs, f = cisco_cfs$year)
 
-post_draw<-post(x = cisco_cfs$day, x.star = cisco_cfs$day, y = cisco_cfs$cfs)
+
+test<-sample_n(cisco_cfs[[1]], 50)
+x.star<-1:365
+# Calculate the covariance matrix
+Sigma <- sq_exp_kernel(x.star,x.star)
+
+prior<-draw(n_draws = 20, x.star, rep(0, length(x.star)), cov = Sigma)
+
+prior%>%ggplot(aes(y = value, x = x, color = as.factor(sample)))+geom_line()
+
+post_params<-post(test$day, 1:365, test$cfs)
 
 
-draws<-draw(20, x = cisco_cfs$day, post_draw$post_mean, cov = post_draw$post_var)
+  ggplot(data = NULL, aes(x = test$day, y = post_params$post_mean))+
+    geom_point()
+
+post<-draw(50, x = x.star, mu = post_params$post_mean, cov = post_params$post_var)
 
 
 ggplot(draws,aes(x=x,y=value)) +
